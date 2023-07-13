c88f82aea65eccefe200491de31fe3a9cf1efebd
Kyle-Deng <1176462710@qq.com>
2023-07-13 22:42:19 +0800

    fix: add environment check

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-if (typeof global.crypto !== 'object') {
-	global.crypto = crypto;
-}
+const isBrowser = typeof window !== 'undefined';
+const isNode = typeof process === 'object';
+let crypto;
+if (isBrowser) {
+	// Browser function here
+} else if (isNode){
+	try {
+		const path = 'crypto';
+		crypto = require(`${path}`);
+	} catch (err) {
+		console.log('crypto support is disabled!');
+	}
 
-if (typeof global.crypto.getRandomValues !== 'function') {
-	global.crypto.getRandomValues = getRandomValues;
-}
+	/**
+	 * @param array
+	 * @return getRandomValues method from crypto.webcrypto
+	 */
+	function getRandomValues(array) {
+		return crypto.webcrypto.getRandomValues(array);
+	}
 
-/**
- * @param array
- */
-function getRandomValues(array) {
-	return crypto.webcrypto.getRandomValues(array);
+	if (typeof global.crypto !== 'object') {
+		global.crypto = crypto;
+	}
+
+	if (typeof global.crypto.getRandomValues !== 'function') {
+		global.crypto.getRandomValues = getRandomValues;
+	}
+} else {
+	// New javascript environment to be supported.
 }
